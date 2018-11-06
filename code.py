005ebc089f6c1cd1f05153be359fb900a4ff72fd
Siddharth Mohite <srmohite@uwm.edu>
2018-11-06 15:27:52 -0600

    Included the correct answer to life

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 
 def do_something():
     something = 6
-    print(something)# this will make it much easier in future problems to see that something is actually happening
+    print(str(something)+" isn't the answer to life")# this will make it much easier in future problems to see that something is actually happening
+    print("42 is the answer to life")
